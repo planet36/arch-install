b490547484342c48b4981a65011607b96e7276fb
planet36 <planet36@users.noreply.github.com>
2022-05-30 16:39:14 -0500

    Remove changes to /etc/profile

--- arch-vbox.bash
+++ arch-vbox.bash
@@ -525,29 +525,6 @@ EOT
         .local/share/{bash,fish}
     # }}}
 
-    printf '\n# Added by %q\n' "$THIS_SCRIPT" >> /etc/profile
-
-    cat <<'EOT' >> /etc/profile
-
-# Prepend user path
-PATH="$HOME/.local/bin:$PATH"
-
-# Set XDG vars
-# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-: "${XDG_CACHE_HOME:=$HOME/.cache}"
-: "${XDG_CONFIG_DIRS:=/etc/xdg}"
-: "${XDG_CONFIG_HOME:=$HOME/.config}"
-: "${XDG_DATA_DIRS:=/usr/local/share/:/usr/share/}"
-: "${XDG_DATA_HOME:=$HOME/.local/share}"
-: "${XDG_STATE_HOME:=$HOME/.local/state}"
-export XDG_CACHE_HOME
-export XDG_CONFIG_DIRS
-export XDG_CONFIG_HOME
-export XDG_DATA_DIRS
-export XDG_DATA_HOME
-export XDG_STATE_HOME
-EOT
-
     # }}}
 
     # {{{ Create new user
